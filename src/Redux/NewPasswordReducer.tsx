261d148fda0e4df000ec8eb8f4abaa006f9f6952
Agitobl <vlad.agito@gmail.com>
2022-06-30 16:27:54 +0300

    create new agitoBranch

--- src/Redux/NewPasswordReducer.tsx
+++ src/Redux/NewPasswordReducer.tsx
@@ -1,20 +1,81 @@
+import { Dispatch } from "redux"
+import { NewPasswordData, recoveryApi } from "../api/Api"
+
 export type InitialStateType = {
+    info: string
+    error: string
+    isLoading: boolean
 }
 
 const initialState: InitialStateType = {
-
+    info: '',
+    error: '',
+    isLoading: false
 }
 
-export const NewasswordReducer = (state: InitialStateType = initialState, action: any): InitialStateType => {
+export const NewPasswordReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
+        case 'SET_INFO':
+            return {...state, info: action.info, error: ''}
+        case 'SET_ERROR': 
+            return {...state, error: action.error}
+        case 'CHANGE_IS_LOADING':
+            return {...state, isLoading: action.isLoading}
         default:
             return {...state}
     }
 }
 
+export const setNewPasswordAC = (info: string): recoveryPasswordACType => ({
+    type: 'SET_INFO',
+    info,
+} as const);
+
+export const setErrorAC = (error: string): setErrorACType => ({
+    type: 'SET_ERROR',
+    error,
+} as const);
+
+export const changeIsLoading = (isLoading: boolean): changeIsLoadingACType => ({
+    type: 'CHANGE_IS_LOADING',
+    isLoading,
+} as const);
+
+export const newPasswordTC = (data: NewPasswordData) => (dispatch: Dispatch) => {
+    dispatch(changeIsLoading(true));
+    recoveryApi.setNewPassword(data)
+        .then(res => {
+            console.log(res);
+            dispatch(setNewPasswordAC(res.info));
+            dispatch(changeIsLoading(false));
+        })
+        .catch(error => {
+            dispatch(changeIsLoading(false));
+            dispatch(setErrorAC(error.response.data.error));
+            setTimeout(() => {
+                dispatch(setErrorAC(''))
+            }, 2000);
+        })
+}
+
+export type recoveryPasswordACType = {
+    type: 'SET_INFO',
+    info: string
+}
+
+export type setErrorACType = {
+    type: 'SET_ERROR',
+    error: string
+}
+
+export type changeIsLoadingACType = {
+    type: 'CHANGE_IS_LOADING',
+    isLoading: boolean
+}
+
 
 // export const initializeAppTC = () => (dispatch: Dispatch) => {
-//
+
 // }
 
 // export const setAppErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const)
@@ -23,5 +84,4 @@ export const NewasswordReducer = (state: InitialStateType = initialState, action
 // export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
 //
 //
-// type ActionsType =
-//     | SetAppErrorActionType
+export type ActionsType = recoveryPasswordACType | setErrorACType | changeIsLoadingACType;
