840e08da221a41e02a643f4705aa8181dffe945b
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-07-01 20:10:39 +0500

    Navlink in Login

--- src/api/app-api.ts
+++ src/api/app-api.ts
@@ -3,8 +3,9 @@ import { RegisterValuesType } from "../Redux/RegistrationReducer";
 
 
 export const instance = axios.create({ 
-    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/', 
-    withCredentials: true, 
+    //baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    baseURL: process.env.REACT_APP_BACK_URL || ' https://neko-back.herokuapp.com/2.0',
+    withCredentials: true,
 })
 
 export const accountAPI = {
