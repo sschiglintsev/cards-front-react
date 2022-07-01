53473784f2bc9934e25026acdf6730c10dfd36e3
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-07-01 20:04:06 +0500

    Navlink in Login

--- src/api/login-api.ts
+++ src/api/login-api.ts
@@ -23,7 +23,8 @@ type ResponseType = {
 }
 
 export const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    //baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    baseURL: process.env.REACT_APP_BACK_URL || ' https://neko-back.herokuapp.com/2.0',
     withCredentials: true,
 })
 
