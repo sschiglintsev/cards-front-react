2b9862c66b9da3e10a75ad958d48b1b63a475476
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-07-06 16:47:56 +0500

    add form cards, apiCards, Reducer Cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import React, { useEffect } from 'react';
+import React  from 'react';
 import style from './App.module.css'
 import {RoutesPage} from "./components/Routes/Routes";
 import {Header} from "./components/Header/Header";
 import Error from './components/Error/Error';
-import { recoveryApi } from './api/Api';
 
 
 function App() { 
 
-    useEffect(() => {
-        recoveryApi.isLogin()
-            .then(res => console.log(res));
-    }, []);
-
-    return (
+      return (
         <div className={style.app}>
             <Header/>
             <RoutesPage/>
